891c14e4eb27669ca1248fb010e24e0b8c76953b
Fernando Veliz <fernado.veliz.21@uda.cl>
2023-11-05 18:02:01 -0300

    arreglo de codigo en insertar

--- insertar.php
+++ insertar.php
@@ -27,12 +27,12 @@ if (!ctype_digit($edad)) {
 }
 
 if (count($errors) > 0) {
-    // Si hay errores, redirige a usuario.php con mensajes de error en la URL
+    
     header("Location: usuario.php?errors=" . implode(",", $errors));
     exit;
 }
 
-// Ahora puedes proceder a la inserción en la base de datos
+
 $codusuario = null;
 $sexo = $_POST['sexo'];
 $fechanac = $_POST['fechanac'];
@@ -42,11 +42,11 @@ $sql = "INSERT INTO usuarios VALUES ('$codusuario', '$usuario', '$contrasena', '
 $query = mysqli_query($con, $sql);
 
 if ($query) {
-    // Redirige a usuario.php con un mensaje de éxito
+    
     header("Refresh: 0;url: usuario.php?success=registro_exitoso");
     echo "<script>alert('Exito');</script>";
 } else {
-    // Redirige a usuario.php con un mensaje de error de la base de datos
+    
     header("Refresh: 0;url: usuario.php?error=base_de_datos");
     echo "<script>alert('error');</script>";
 }
